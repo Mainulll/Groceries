3e32745d0dbac7b7f9a2ac73e60d81ff30979d41
Mainulll <minullokuliyana@hotmail.com>
2023-11-06 23:54:17 +1100

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.woolworths.com.au/shop/browse/fruit-veg'
+url = 'https://www.coles.com.au/browse/fruit-vegetables?pid=homepage_cat_explorer_fruit_vege'
 
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
+    products = soup.findAll('div', class_='LinesEllipsis  product__title')
 
-    # txt
+    for product in products:
+        title = product.find('span', class_='LinesEllipsis  product__title').text
+        price = product.find('span', class_='price__value').text
+        print(title, price)
+else:
+        print('Failed to connect to the website')
+
     
