b811f2f3e503cfcda1f63297ee4473ebb0c21f2f
Mainulll <minullokuliyana@hotmail.com>
2023-11-06 22:00:42 +1100

    new

---
+++ import requests.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.woolworths.com.au/shop/browse/fruit-veg'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    
